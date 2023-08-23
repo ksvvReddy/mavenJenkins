6c6fb29b7ffe707ed8d18271eff7e05fb83a6a80
ksvvReddy <vardhanreddy071094@gmail.com>
2023-08-23 11:40:39 +0530

    first commit

---
+++ src/main/java/JenkinsProject/Jenkinsone/Multiple_windows.java
@@ -0,0 +1,52 @@
+package JenkinsProject.Jenkinsone;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Multiple_windows {
+
+	@Test
+	public void check() throws Exception {
+		// TODO Auto-generated method stub
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://demo.guru99.com/popup.php");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[text()='Click Here']")).click();
+		Thread.sleep(2000);
+		String MainWindow = driver.getWindowHandle();
+		System.out.println(MainWindow);
+
+		// To handle all new opened window.
+		Set<String> s1 = driver.getWindowHandles();
+		Iterator<String> i1 = s1.iterator();
+
+		while (i1.hasNext()) {
+			String ChildWindow = i1.next();
+			System.out.println(ChildWindow);
+
+			if (!MainWindow.equalsIgnoreCase(ChildWindow)) {
+
+				// Switching to Child window
+				driver.switchTo().window(ChildWindow);
+				driver.findElement(By.name("emailid")).sendKeys("vardhanreddy071094@gmail.com");
+
+				driver.findElement(By.name("btnLogin")).click();
+
+				// Closing the Child Window.
+				driver.close();
+			}
+		}
+		// Switching to Parent window i.e Main Window.
+		driver.switchTo().window(MainWindow);
+		driver.quit();
+	}
+}
